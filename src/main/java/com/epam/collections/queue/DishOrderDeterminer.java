4f9f72bb6f611b3e54f228f2f6ae4c5b7e6682e5
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-04-24 10:03:21 +0500

    HI

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,22 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> integerList=new LinkedList<>();
+
+        for (int i=1;i<=numberOfDishes;i++){
+            if (i==everyDishNumberToEat){
+             integerList.add(0,i);
+            }
+            else integerList.add(i);
+
+        }
+
+        System.out.println(numberOfDishes+"   "+everyDishNumberToEat);
+
+        return integerList;
+
     }
 }
